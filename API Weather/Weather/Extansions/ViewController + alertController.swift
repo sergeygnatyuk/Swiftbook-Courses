d90b1cd086c52198fbd89703b3a3b8c84911ff5c
Sergey <Starcom12@mail.ru>
2021-04-08 23:29:23 +0300

    Restructure

--- API Weather/Weather/Extansions/ViewController + alertController.swift
+++ API Weather/Weather/Extansions/ViewController + alertController.swift
@@ -11,13 +11,13 @@ extension ViewController {
     func presentSearchAlertController(withTitle title: String?, message: String?,
                                       style: UIAlertController.Style,
                                       completionHandler: @escaping (String) -> Void ) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
-        alertController.addTextField { textFieldFirst in
+        let ac = UIAlertController(title: title, message: message, preferredStyle: style)
+        ac.addTextField { tf in
             let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
-            textFieldFirst.placeholder = cities.randomElement()
+            tf.placeholder = cities.randomElement()
         }
         let search = UIAlertAction(title: "Search", style: .default) { action in
-            let textField = alertController.textFields?.first
+            let textField = ac.textFields?.first
             guard let cityName = textField?.text else { return }
             if cityName != "" {
 //                self.networkWeatherManager.fetchCurrentWeather(fortCity: cityName)
@@ -27,9 +27,9 @@ extension ViewController {
         }
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
-        alertController.addAction(search)
-        alertController.addAction(cancel)
-        present(alertController, animated: true, completion: nil)
+        ac.addAction(search)
+        ac.addAction(cancel)
+        present(ac, animated: true, completion: nil)
     }
 }
 
--- API Weather/Weather/Model/NetworkWeatherManager.swift
+++ API Weather/Weather/Model/NetworkWeatherManager.swift
@@ -6,31 +6,43 @@
 //
 
 import Foundation
-protocol NetworkWeatherManagerDelegate: class {
-    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
-}
+import CoreLocation
 
 class NetworkWeatherManager {
     
-   weak var delegate:NetworkWeatherManagerDelegate?
+    enum RequestType {
+        case cityName(city: String)
+        case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    }
     
-    func fetchCurrentWeather(fortCity city: String) {
-   
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)"
-        guard let url = URL(string: urlString) else {return}
+    var onCompletion: ((CurrentWeather) -> Void)?
+    
+    func fetchCurrentWeather(forRequestType requestType: RequestType) {
+        var urlString = ""
+        switch requestType {
+        case .cityName(let city):
+          urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)&units=metric"
+        case .coordinate(let latitude, let longitude):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&apikey=\(apiKey)&units=metric"
+        }
+        performRequest(withURLString: urlString)
+    }
+    
+    
+   fileprivate func performRequest(withURLString urlString: String) {
+        guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
                 if let currentWeather = self.parseJSON(withData: data) {
-                    self.delegate?.updateInterface(self, with: currentWeather)
+                    self.onCompletion?(currentWeather)
                 }
-               
             }
         }
         task.resume()
     }
     
-    func parseJSON(withData data: Data) -> CurrentWeather? {
+   fileprivate func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
        let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
