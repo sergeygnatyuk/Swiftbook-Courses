262400599bbdcf8a2b48ee305d80f8ed3aa57628
Sergey <Starcom12@mail.ru>
2021-05-07 23:51:47 +0300

    Added the display of the greeting in the class UserProfileVC

--- Networking/Networking/Controllers/UserProfileViewController/ExtensionUserProfileVC.swift
+++ Networking/Networking/Controllers/UserProfileViewController/ExtensionUserProfileVC.swift
@@ -19,20 +19,26 @@ extension UserProfileVC {
         
         if Auth.auth().currentUser != nil {
             
-            guard let uid = Auth.auth().currentUser?.uid else { return }
-            
-            Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            if let userName = Auth.auth().currentUser?.displayName {
+                activityIndicator.stopAnimating()
+                userNameLabel.isHidden = false
+                userNameLabel.text = getProviderData(with: userName)
+            } else {
+                guard let uid = Auth.auth().currentUser?.uid else { return }
                 
-                guard let userData = snapshot.value as? [String: Any] else { return }
-                
-                self.currentUser = CurrentUser(uid: uid, data: userData)
-                
-                self.activityIndicator.stopAnimating()
-                self.userNameLabel.isHidden = false
-                self.userNameLabel.text = self.getProviderData()
-                
-            }) { (error) in
-                print(error)
+                Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+                    
+                    guard let userData = snapshot.value as? [String: Any] else { return }
+                    
+                    self.currentUser = CurrentUser(uid: uid, data: userData)
+                    
+                    self.activityIndicator.stopAnimating()
+                    self.userNameLabel.isHidden = false
+                    self.userNameLabel.text = self.getProviderData(with: self.currentUser?.name ?? "NoName")
+                    
+                }) { (error) in
+                    print(error)
+                }
             }
         }
     }
@@ -56,7 +62,7 @@ extension UserProfileVC {
         }
     }
     
-    private func getProviderData() -> String {
+    private func getProviderData(with user: String) -> String {
         var greetings = ""
         
         if let providerData = Auth.auth().currentUser?.providerData {
@@ -68,11 +74,13 @@ extension UserProfileVC {
                     provider = "Facebook"
                 case "google.com":
                     provider = "Google"
+                case "password":
+                    provider = "Email"
                 default:
                     break
                 }
             }
-            greetings = "\(currentUser?.name ?? "Noname") Logged in with \(provider!)"
+            greetings = "\(user) Logged in with \(provider!)"
         }
         return greetings
     }
@@ -90,6 +98,10 @@ extension UserProfileVC {
                 case "google.com":
                     GIDSignIn.sharedInstance()?.signOut()
                     print("Used did log out of Google")
+                    openLoginViewController()
+                case "password":
+                    try!Auth.auth().signOut()
+                    print("User did sign out")
                     openLoginViewController()
                 default:
                     print("User is signed in with \(userInfo.providerID)")
